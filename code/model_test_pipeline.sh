eead8b39bbef49c46a6cbc6456314d666f97fdba
Luotting <luo_tt@tongji.edu.cn>
2025-08-07 17:20:36 +0800

    Update model_test_pipeline.sh

--- code/model_test_pipeline.sh
+++ code/model_test_pipeline.sh
@@ -17,13 +17,16 @@ xpore=path_to_xpore
 SingleMod=path_to_SingleMod
 Dinopore=path_to_Dinopore
 pum6a=path_to_pum6a
-
+dorado=path_to_dorado
+modkit=path_to_modkit
+xron=path_to_xron
 ws=path_to_work
 sample=sample_name
 
 ko_single=path_to_save_single_fast5_files
 wt_single=path_to_save_single_fast5_files
-
+wt_pod5=path_to_pod5_files
+ko_pod5=path_to_pod5_files
 wt_basecall=path_to_save_basecalled_fast5_files
 ko_basecall=path_to_save_basecalled_fast5_files
 
@@ -130,7 +133,7 @@ m6anet inferencee --input_dir $ws/m6Anet/dataprep \
 --out_dir $ws/m6Anet/result \
 --n_processes 16
 
-##7.tombo¡ª¡ª5mc/m6A
+##7.tomboÂ¡ÂªÂ¡Âª5mc/m6A
 ##m6A
 cd $ws/Tombo
 
@@ -485,6 +488,10 @@ python -m m6Aiso molecular_m6A_predication --model_path $m6Aiso/module/semi_mode
         --using_signal_filename $ws/m6Aiso/Candidatecurrent.tsv --predict_result_filename $ws/m6Aiso/m6A_prob.txt \
         --max_value_filename $m6Aiso/data/merge_max_value_list.txt \
         --min_value_filename $m6Aiso/data/merge_min_value_list.txt
-
-
+##22.dorado
+$dorado/bin/dorado basecaller $dorado/rna004_130bps_sup@v3.0.1  $wt_pod5 --reference $ref --reference $ref --modified-bases m6A_DRACH >  $ws/wt_basecall/wt.bam
+$modkit pileup $ws/wt_basecall/wt.bam $ws/wt.dorado.bed
+##23.xron
+$xron call -i $ws/wt_single/workspace -o $ws/wt_basecall --fast5 -m $xron/models/RNA004 --device cuda --batch_size 200
 #apply the same operation in ko
+
